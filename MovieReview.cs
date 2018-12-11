17ceafa4de4ac1cbbc49b5750246cbcbee186a2d
AbhishekLohit <abhishek1.lohit@gmail.com>
2018-12-11 23:17:51 +0530

    Add files via upload

---
+++ MovieReview.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMDBApp.Models
+{
+    public class MovieReview
+    {
+        public string MovieCode { get; set; } 
+
+        public string MovieName { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public string genre { get; set; }
+
+        public string Performances { get; set; }
+
+        public string Direction { get; set; }
+      
+        public string Editing { get; set; }
+       
+        public string Dialogue { get; set; }
+
+        public string Writing { get; set; }
+
+        public string ActionStatus { get; set; }
+
+        public string Music { get; set; }
+    }
+}
---
+++ Movies2018.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMDBApp.Models
+{
+    public class Movies2018
+    {
+        public int MovieCode { get; set; }
+
+        public string MovieName { get; set; }
+
+        public  int ReleaseYear { get; set; }
+
+        public string genre { get; set; }
+
+        public int Runtime { get; set; }
+
+        public double IMDBRating { get; set; }
+
+        public string MovieDetails { get; set; }
+
+        public string Director { get; set; }
+
+        public string Actor { get; set; }
+
+        public double BoxOfficeCollection { get; set; }
+
+        public string ImageId { get; set; }
+
+
+    }
+
+}
